3f81a4521386ba69da1d2c4855ccac9b658a62b4
6498314-stack <6498314@gmail.com>
2026-06-25 12:35:51 +0300

    Switch site images to server-hosted updated exterior photos

--- src/images.ts
+++ src/images.ts
@@ -1,12 +1,5 @@
-// @ts-ignore
-import bcStreetView from './assets/images/bc_street_view_1779868807492.png';
-// @ts-ignore
-import bcCourtyard from './assets/images/bc_courtyard_1779868829326.png';
-// @ts-ignore
-import bcOfficeInterior from './assets/images/bc_office_interior_1779868851447.png';
-
 export const IMAGES = {
-  streetView: bcStreetView,
-  courtyard: bcCourtyard,
-  officeInterior: bcOfficeInterior,
+  streetView: '/images/site/exterior-main-01.webp',
+  courtyard: '/images/site/courtyard-02.webp',
+  officeInterior: '/images/site/courtyard-wide-01.webp',
 };
